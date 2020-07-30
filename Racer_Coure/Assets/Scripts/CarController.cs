e262259c7cd3ea37489a3812836fcb2ad6bfd745
Noah Day <sheltielover432@gmail.com>
2020-07-29 23:19:59 -0400

    Steering

--- Racer_Coure/Assets/Scripts/CarController.cs
+++ Racer_Coure/Assets/Scripts/CarController.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class CarController : MonoBehaviour
 {
     public Rigidbody theRB;
-    public float maxSpeed, forwardAcceleration = 8f, reverseAcceleration = 4f;
-    private float speedInput;
+    public float maxSpeed, forwardAcceleration = 8f, reverseAcceleration = 4f, turnStrength = 180f;
+    private float speedInput, turnInput;
     // Start is called before the first frame update
     void Start()
     {
@@ -25,11 +25,22 @@ public class CarController : MonoBehaviour
         {
             speedInput = Input.GetAxis("Vertical") * reverseAcceleration;
         }
+
+        turnInput = Input.GetAxis("Horizontal");
+        if (Input.GetAxis("Vertical") != 0)
+        {
+            transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3(0f, turnInput * turnStrength  * Time.deltaTime * Mathf.Sign(speedInput) * (theRB.velocity.magnitude/maxSpeed), 0f));
+        }
         transform.position = theRB.position;
     }
 
     private void FixedUpdate()
     {
-        theRB.AddForce(new Vector3(0, 0f, speedInput * 1000));
+        theRB.AddForce(transform.forward * speedInput * 1000f);
+        if (theRB.velocity.magnitude > maxSpeed)
+        {
+            theRB.velocity = theRB.velocity.normalized * maxSpeed;
+        }
+        Debug.Log(theRB.velocity.magnitude);
     }
 }
