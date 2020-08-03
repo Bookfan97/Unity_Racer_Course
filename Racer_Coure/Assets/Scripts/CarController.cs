a4a0d2d10e98ac80690439b693c7d141a30ae8bd
Noah Day <sheltielover432@gmail.com>
2020-08-02 23:40:17 -0400

    Checkpoints

--- Racer_Coure/Assets/Scripts/CarController.cs
+++ Racer_Coure/Assets/Scripts/CarController.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class CarController : MonoBehaviour
 {
     public Rigidbody theRB;
-
     public float maxSpeed,
         forwardAcceleration = 8f,
         reverseAcceleration = 4f,
@@ -22,6 +21,8 @@ public class CarController : MonoBehaviour
     public LayerMask whatIsGround;
     public ParticleSystem[] dustTrail;
     public AudioSource engineSound, skidSound;
+    private int nextCheckpoint;
+    public int currentLap;
     // Start is called before the first frame update
     void Start()
     {
@@ -127,6 +128,20 @@ public class CarController : MonoBehaviour
         if (grounded && Input.GetAxis("Vertical") != 0) 
         {
             transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3(0f, turnInput * turnStrength  * Time.deltaTime * Mathf.Sign(speedInput) * (theRB.velocity.magnitude/maxSpeed), 0f)); 
+        }
+    }
+
+    public void CheckpointHit(int checkpointNumber)
+    {
+        //Debug.Log(checkpointNumber);
+        if (checkpointNumber == nextCheckpoint)
+        {
+            nextCheckpoint++;
+            if (nextCheckpoint == RaceManager.instance.Checkpoints.Length)
+            {
+                nextCheckpoint = 0;
+                currentLap++;
+            }
         }
     }
 }
---
+++ Racer_Coure/Assets/Scripts/RaceManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaceManager : MonoBehaviour
+{
+    public static RaceManager instance;
+    public Checkpoint[] Checkpoints;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < Checkpoints.Length; i++)
+        {
+            Checkpoints[i].checkpointNumber = i;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
