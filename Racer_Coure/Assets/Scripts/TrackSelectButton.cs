6ae3dec970c93cf39bdef62d323707a98c8d3cc3
Noah Day <sheltielover432@gmail.com>
2020-08-05 21:25:14 -0400

    Race setup

---
+++ Racer_Coure/Assets/Scripts/TrackSelectButton.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TrackSelectButton : MonoBehaviour
+{
+    public string trackSceneName;
+    public Image TrackImage;
+    public int raceLaps = 4;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SelectTrack()
+    {
+        RaceInfoManager.instance.trackToLoad = trackSceneName;
+        RaceInfoManager.instance.noOfLaps = raceLaps;
+        MainMenu.instance.trackSelectImage.sprite = TrackImage.sprite;
+        RaceInfoManager.instance.trackSprite = TrackImage.sprite;
+        MainMenu.instance.CloseTrackSelect();
+    }
+}
